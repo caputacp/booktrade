a3d62baf0d62e78307363edc7c0dcd835502aed9
caputacp <tuposacp@gmail.com>
2018-05-05 17:23:25 +1000

    Improvements

--- sourceCode/public_html/functions.php
+++ sourceCode/public_html/functions.php
@@ -170,7 +170,7 @@ function getMessage($msgId){
 }
 
 function sendEmailTo($email){
-  $msg = "Welcome to Book.Trade!\n\n Please verify your account by clicking the link below \n http://booktrade.duckdns.org";
+  $msg = "Welcome to Book.Trade!\n\n Please verify your account by clicking the link below \n http://booktrade.duckdns.org#loginContent";
   $sendMail = mail($email, "Booktrade Verification", $msg, "From: booktradedotcom@gmail.com");
   return $sendMail;
 
